c6ac2489be6027d264578a9772eaa9ecc04bfaf1
Miura55 <acordion.piano@gmail.com>
2025-01-29 01:58:31 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# AWS Face Authenticator
+AWS Rekognitionを使用した簡易的な顔認証システムです。
+
+## 実行環境
+- Python3
+- Boto3(>=1.34.7)
+
+## 用意する画像
+- 顔認証を行う画像(source.jpg)
+- 顔認証の対象となる画像(target.jpg)
---
+++ tutorial.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+def compare_faces(sourceFile, targetFile):
+
+    session = boto3.Session()
+    client = session.client('rekognition')
+
+    imageSource = open(sourceFile, 'rb')
+    imageTarget = open(targetFile, 'rb')
+
+    response = client.compare_faces(SimilarityThreshold=80,
+                                    SourceImage={'Bytes': imageSource.read()},
+                                    TargetImage={'Bytes': imageTarget.read()})
+
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        similarity = str(faceMatch['Similarity'])
+        print('The face at ' +
+              str(position['Left']) + ' ' +
+              str(position['Top']) +
+              ' matches with ' + similarity + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()
+    return len(response['FaceMatches'])
+
+if __name__ == "__main__":
+    source_file = 'source.jpg'
+    target_file = 'target.jpg'
+    face_matches = compare_faces(source_file, target_file)
+    print("Face matches: " + str(face_matches))
